f408f8c14fbaa7f25cea72aeb4a7bc183dac7b77
Emma Mattner <emattner1997@gmail.com>
2021-07-28 22:56:18 +0930

    long and lat openweather

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var searchList = [];
+const searchList = [];
 
 $("#card1").hide();
 $("#card2").hide();
@@ -6,27 +6,32 @@ $("#card3").hide();
 $("#card4").hide();
 $("#card5").hide();
 
-var today = new Date();
-var day = String(today.getDate());
-var month = String(today.getMonth() + 1);
-var year = today.getFullYear();
-var displayDate = day + "/" + month + "/" + year;
+const today = new Date();
+const  day = String(today.getDate());
+const month = String(today.getMonth() + 1);
+const year = today.getFullYear();
+const displayDate = day + "/" + month + "/" + year;
 
 function displayCityWeather() {
     event.preventDefault();
 
-    var citySearch = $("#citySearchBar").val();
-    var queryURLCWD = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=48eb8f7025236f284142f7fe0b9f55b4&units=metric";
+    const citySearch = $("#citySearchBar").val();
+    const queryURLCWD = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=48eb8f7025236f284142f7fe0b9f55b4&units=metric";
 
     $.ajax({
         url: queryURLCWD,
         method: "GET"
     }).then(function (response) {
         $("#cityName").text(response.name + " " + displayDate);
-        var tempNumber = Math.round(response.main.temp);
+        const tempNumber = Math.round(response.main.temp);
         $("#temperature").text("Temperature: " + tempNumber + "°C");
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
         $("#windSpeed").text("Wind Speed: " + response.wind.speed + " KM/H");
+        
+        const long = response.coord.lon;
+        const lat = response.coord.lat;
+
+        const queryURLUVIndex = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=48eb8f7025236f284142f7fe0b9f55b4";
 
 
 }
