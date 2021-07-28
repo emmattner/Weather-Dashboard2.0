4743a1ce8881c0c9803f0d37942bc1e4227e0bd0
Emma Mattner <emattner1997@gmail.com>
2021-07-28 22:40:09 +0930

    updating syntax

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ $("#card2").hide();
 $("#card3").hide();
 $("#card4").hide();
 $("#card5").hide();
+
+var today = new Date();
+var day = String(today.getDate());
+var month = String(today.getMonth() + 1);
+var year = today.getFullYear();
+var displayDate = day + "/" + month + "/" + year;
