c98ac0020f4502744b3dc06ff94fee426d5908ae
Emma Mattner <emattner1997@gmail.com>
2021-07-28 22:52:23 +0930

    updated waether request

--- script.js
+++ script.js
@@ -14,4 +14,19 @@ var displayDate = day + "/" + month + "/" + year;
 
 function displayCityWeather() {
     event.preventDefault();
+
+    var citySearch = $("#citySearchBar").val();
+    var queryURLCWD = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=48eb8f7025236f284142f7fe0b9f55b4&units=metric";
+
+    $.ajax({
+        url: queryURLCWD,
+        method: "GET"
+    }).then(function (response) {
+        $("#cityName").text(response.name + " " + displayDate);
+        var tempNumber = Math.round(response.main.temp);
+        $("#temperature").text("Temperature: " + tempNumber + "°C");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#windSpeed").text("Wind Speed: " + response.wind.speed + " KM/H");
+
+
 }
